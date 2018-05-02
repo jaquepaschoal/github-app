9a168fa54557e7373155e1e0a1bb4c6bd3986ba4
jaquepaschoal <jaque-paschoal@hotmail.com>
2018-05-02 13:22:53 -0300

    create structure

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        Rodando
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
