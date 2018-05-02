3fe62d1ac381bf94f787be2e6bb05d105e8b2c39
jaquepaschoal <jaque-paschoal@hotmail.com>
2018-05-02 13:41:03 -0300

    app marking

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,43 @@ import React from 'react';
 class AppComponent extends React.Component {
   render() {
     return (
-      <div>
-        Rodando
+      <div className='app'>
+        <div className='search'>
+          <input type='search' placeholder='Digite o nome do usuário no Github'/>
+        </div>
+
+        <div className='user-info'>
+          <img src='https://avatars3.githubusercontent.com/u/28562703?v=4'/>
+          <h1>
+            <a href='https://github.com/jaquepaschoal'>Jaqueline Paschoal</a>
+          </h1>
+
+          <ul className='repos-info'>
+            <li>Repositórios: 13 </li>
+            <li>Seguidores: 0 </li>
+            <li>Seguindo: 0 </li>
+          </ul>
+
+          <div className='actions'>
+            <button>Repositórios</button>
+            <button>Favoritos</button>
+          </div>
+
+          <div className='repos'>
+            <h2>Repositórios:</h2>
+            <ul >
+              <li><a href='#'>Nome do repositório</a></li>
+            </ul>
+          </div>
+
+          <div className='starred'>
+            <h2>Favoritos:</h2>
+            <ul >
+              <li><a href='#'>Nome do repositório</a></li>
+            </ul>
+          </div>
+
+        </div>
       </div>
     );
   }
